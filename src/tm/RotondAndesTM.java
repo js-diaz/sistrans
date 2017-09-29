5543d457c4d64a6eef99f86e5b32a0064acedbd2
Sergio Guzmán Mayorga <supersergio.sgm@gmail.com>
2017-09-29 17:04:24 -0500

    Establecí el requerimiento de consulta 3. Ya está con rest. Después de esto hago pruebas mañana y miro como se harían los multi criterio

--- src/tm/RotondAndesTM.java
+++ src/tm/RotondAndesTM.java
@@ -18,6 +18,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
@@ -33,6 +34,7 @@ import vos.Producto;
 import vos.Producto.TiposDePlato;
 import vos.Restaurante;
 import vos.Usuario;
+import vos.UsuarioCompleto;
 import vos.UsuarioMinimum.Rol;
 import vos.Video;
 import vos.Zona;
@@ -2845,6 +2847,48 @@ public class RotondAndesTM {
 		return u;
 	}
 	/**
+	 * Busca al usuario por id.<br>
+	 * @param id Id del usuario.<br>
+	 * @return Usuario buscado.<br>
+	 * @throws Exception Si existe algún tipo de error
+	 */
+	public UsuarioCompleto usuarioCompletoBuscarUsuarioPorId(Long id) throws Exception
+	{
+		UsuarioCompleto u =null;
+		DAOTablaUsuario dao = new DAOTablaUsuario();
+		try
+		{
+			this.conn=darConexion();
+			dao.setConn(conn);
+			u=dao.darTodaLaInfoDeUnCliente(id);
+		}
+		catch (SQLException e) {
+			System.err.println("SQLException:" + e.getMessage());
+			e.printStackTrace();
+			throw e;
+		} 
+		catch (Exception e) {
+			System.err.println("GeneralException:" + e.getMessage());
+			e.printStackTrace();
+			throw e;
+		}
+		finally
+		{
+			try
+			{
+				dao.cerrarRecursos();
+				if(this.conn!=null) this.conn.close();
+			}
+			catch(SQLException exception)
+			{
+				System.err.println("SQLException closing resources:" + exception.getMessage());
+				exception.printStackTrace();
+				throw exception;
+			}
+		}
+		return u;
+	}
+	/**
 	 * Agrega un usuario al sistema.<br>
 	 * @param usuario Usuario a agregar.<br>
 	 * @throws Exception Si existe algún tipo de error
@@ -3259,44 +3303,7 @@ public class RotondAndesTM {
 			return videos;
 		}
 
-		/**
-		 * Metodo que modela la transaccion que busca el/los videos en la base de datos con el nombre entra como parametro.
-		 * @param name - Nombre del video a buscar. name != null
-		 * @return ListaVideos - objeto que modela  un arreglo de videos. este arreglo contiene el resultado de la busqueda
-		 * @throws Exception Si existe algún tipo de error -  cualquier error que se genere durante la transaccion
-		 */
-		public List<Video> buscarVideosPorName(String name) throws Exception {
-			List<Video> videos;
-			DAOTablaVideos daoVideos = new DAOTablaVideos();
-			try 
-			{
-				//////transaccion
-				this.conn = darConexion();
-				daoVideos.setConn(conn);
-				videos = daoVideos.buscarVideosPorName(name);
 
-			} catch (SQLException e) {
-				System.err.println("SQLException:" + e.getMessage());
-				e.printStackTrace();
-				throw e;
-			} catch (Exception e) {
-				System.err.println("GeneralException:" + e.getMessage());
-				e.printStackTrace();
-				throw e;
-			} finally {
-				try {
-					daoVideos.cerrarRecursos();
-					if(this.conn!=null)
-						this.conn.close();
-				} catch (SQLException exception) {
-					System.err.println("SQLException closing resources:" + exception.getMessage());
-					exception.printStackTrace();
-					throw exception;
-				}
-			}
-			return videos;
-		}
-	
 	//VIDEOANDES-EJEMPLO
 	/**
 	 * Metodo que modela la transaccion que retorna todos los videos de la base de datos.
@@ -3566,7 +3573,7 @@ public class RotondAndesTM {
 	
 	public static void main(String[] args) throws SQLException,Exception {
 		RotondAndesTM tm = new RotondAndesTM("./WebContent/WEB-INF/ConnectionData");
-		System.out.println(tm.rolBuscarRolsPorName("PROVEEDOR"));
+		System.out.println(Arrays.toString(tm.usuarioCompletoBuscarUsuarioPorId(2l).getFrecuencias()));
 		
 	}
 
