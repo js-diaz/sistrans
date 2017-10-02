343d0d92ae8ad27f50a3f7784f71e8cdddd4d4c9
Juan Sebastian Diaz <js.diaz@uniandes.edu.co>
2017-10-02 12:08:16 -0500

    algunos sql de inicializacion

---
+++ Docs/inicializacion/ZONA.sql
@@ -0,0 +1,15 @@
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Sports', 88, 1, 0, 37);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Computers', 17, 1, 0, 57);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Jewelery', 45, 0, 0, 29);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Shoes', 40, 0, 0, 91);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Garden', 37, 1, 0, 26);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Games', 11, 0, 0, 19);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Beauty', 25, 0, 0, 68);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Outdoors', 44, 0, 0, 4);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Kids', 56, 0, 1, 45);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Clothing', 48, 1, 1, 55);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Toys', 44, 0, 0, 98);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Home', 35, 1, 0, 18);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Music', 27, 1, 0, 67);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Books', 25, 0, 0, 64);
+insert into ZONA (nombre, capacidad, ingresoespecial, abiertaactualmente, capacidadocupada) values ('Baby', 96, 1, 1, 6);
